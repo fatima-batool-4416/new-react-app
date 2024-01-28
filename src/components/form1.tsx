6aa5fbe5909bc9af787ec338b1fdff922f6b7fff
fatima-batool-4416 <fatima.batool.4416@gmail.com>
2024-01-29 02:21:56 +0500

    forms

--- src/components/form1.tsx
+++ src/components/form1.tsx
@@ -1,28 +1,41 @@
+// src/components/forms/Form1.tsx
 import React from 'react';
 import styled from 'styled-components';
+import { Form, Input, Button } from 'antd';
 
-const FormContainer = styled.form`
+const FormContainer = styled(Form)`
+  /* Add your styling here */
   padding: 20px;
   border: 1px solid #ddd;
   border-radius: 8px;
   margin-bottom: 20px;
 `;
 
-const SubmitButton = styled.button`
-  background-color: #4caf50;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
 const Form1: React.FC = () => {
   return (
     <FormContainer>
       <h2>Form 1</h2>
-      {/* Add form fields here */}
-      <SubmitButton type="submit">Submit Form 1</SubmitButton>
+      <Form.Item label="Field 1" name="field1" rules={[{ required: true, message: 'Please input Field 1!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 2" name="field2" rules={[{ required: true, message: 'Please input Field 2!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 3" name="field3" rules={[{ required: true, message: 'Please input Field 3!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 4" name="field4" rules={[{ required: true, message: 'Please input Field 4!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 5" name="field5" rules={[{ required: true, message: 'Please input Field 5!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Button type="primary" htmlType="submit">Submit Form 1</Button>
     </FormContainer>
   );
 };
--- src/components/form2.tsx
+++ src/components/form2.tsx
@@ -1,7 +1,9 @@
+// src/components/forms/Form2.tsx
 import React from 'react';
 import styled from 'styled-components';
+import { Form, Input, Button } from 'antd';
 
-const FormContainer = styled.form`
+const FormContainer = styled(Form)`
   /* Add your styling here */
   padding: 20px;
   border: 1px solid #ddd;
@@ -9,22 +11,31 @@ const FormContainer = styled.form`
   margin-bottom: 20px;
 `;
 
-const SubmitButton = styled.button`
-  /* Add your button styling here */
-  background-color: #4caf50;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
 const Form2: React.FC = () => {
   return (
     <FormContainer>
       <h2>Form 2</h2>
-      {/* Add form fields here */}
-      <SubmitButton type="submit">Submit Form 2</SubmitButton>
+      <Form.Item label="Field 6" name="field6" rules={[{ required: true, message: 'Please input Field 6!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 7" name="field7" rules={[{ required: true, message: 'Please input Field 7!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 8" name="field8" rules={[{ required: true, message: 'Please input Field 8!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 9" name="field9" rules={[{ required: true, message: 'Please input Field 9!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Field 10" name="field10" rules={[{ required: true, message: 'Please input Field 10!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Button type="primary" htmlType="submit">Submit Form 2</Button>
     </FormContainer>
   );
 };
